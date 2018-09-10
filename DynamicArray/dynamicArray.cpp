321b5afbe6f8d1ed6187347ce1fd4ff77872b983
MatRenaud <32044959+MatRenaud@users.noreply.github.com>
2018-09-10 14:22:44 -0400

    1 test done

--- DynamicArray/dynamicArray.cpp
+++ DynamicArray/dynamicArray.cpp
@@ -1,1 +1,34 @@
 #include "dynamicArray.h"
+
+DynamicArray::DynamicArray(){}
+
+DynamicArray::DynamicArray(int newCapacite)
+{
+	capacite = newCapacite;
+	tabElements= new int[capacite];
+	for (int i = 0; i < capacite; i++) {
+		tabElements[i] = 0;
+	}
+}
+
+int DynamicArray::getCapacite()
+{
+	return capacite;
+}
+
+int DynamicArray::setCapacite()
+{
+	return 0;
+}
+
+int DynamicArray::getElement(int i)
+{
+	return tabElements[i];
+}
+
+int DynamicArray::setElement()
+{
+	return 0;
+}
+
+
--- DynamicArray/dynamicArray.h
+++ DynamicArray/dynamicArray.h
@@ -1,18 +1,19 @@
-#ifndef DYNAMICARRAY_H_
-#define DYNAMICARRAY_H_
-
+#pragma once
 
 class DynamicArray
 {
-
 public:
-
-
-
+	DynamicArray();
+	DynamicArray(int capacite);
+	int getCapacite();
+	int setCapacite();
+	int getElement(int i);
+	int setElement();
+	
 private:
-
-
+	const int DEFAULT_CAPACITY = 100;
+	int capacite = DEFAULT_CAPACITY;
+	int* tabElements;
 };
 
 
-#endif //DYNAMICARRAY_H_
